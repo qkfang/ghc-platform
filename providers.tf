253e923b4cd3fd271287da72ba81238ad437feb6
Daniel Fang <qkfang@hotmail.com>
2026-02-12 09:49:14 +1100

    add

--- providers.tf
+++ providers.tf
@@ -8,15 +8,17 @@ terraform {
     }
   }
 
-  backend "azurerm" {
-    # Configure these values in your GitHub Actions secrets or backend config file
-    # resource_group_name  = "tfstate-rg"
-    # storage_account_name = "tfstateXXXXX"
-    # container_name       = "tfstate"
-    # key                  = "webapp.tfstate"
-  }
+  # backend "azurerm" {
+  #   # Configure these values in your GitHub Actions secrets or backend config file
+  #   # resource_group_name  = "tfstate-rg"
+  #   # storage_account_name = "tfstateXXXXX"
+  #   # container_name       = "tfstate"
+  #   # key                  = "webapp.tfstate"
+  # }
 }
 
 provider "azurerm" {
   features {}
+  skip_provider_registration = true
+  # use_msi = true  # Enable for managed identity in Azure environments
 }
